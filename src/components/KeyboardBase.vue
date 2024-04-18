5534a45adb41501a60c09cca98a71598f90f532a
Оксана <oksana5566@yandex.ru>
2024-04-18 16:43:47 +0500

    Добавляет клавиатуру для ввода числа

---
+++ src/components/KeyboardBase.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+// import { ref, computed, type Ref } from 'vue'
+
+const emit = defineEmits<{
+  add: [number: number]
+  remove: []
+}>()
+
+const numbers: number[] = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+
+// const count: Ref<number> = ref(0)
+
+// const disabledButton = computed<boolean>(() => count.value === 0)
+
+// function increment(): void {
+//   count.value++
+// }
+
+// function decrement(): void {
+//   if (count.value > 0) {
+//     count.value--
+//   }
+// }
+
+// function addToCart(): void {
+//   emit('addToCart', count.value)
+//   count.value = 0
+// }
+
+// const open: Ref<boolean> = ref(false)
+
+// function openKeyboard() {
+//   open.value = true
+// }
+</script>
+
+<template>
+  <div class="keyboard">
+    <div class="keyboard-numbers">
+      <button
+        v-for="num in numbers"
+        :key="num"
+        class="button button--square"
+        @click="emit('add', num)"
+      >
+        {{ num }}
+      </button>
+    </div>
+
+    <button class="button keyboard-remove" @click="emit('remove')">&lt; &mdash;</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.keyboard {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  width: calc(122px + 4em);
+  background: var(--color-background);
+  border: 1px solid var(--color-border);
+  padding: 1em;
+
+  &-numbers {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1em;
+  }
+}
+</style>
--- src/components/catalog/CatalogActions.vue
+++ src/components/catalog/CatalogActions.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref, computed, type Ref } from 'vue'
 import CartIcon from '@/components/icons/CartIcon.vue'
+import KeyboardIcon from '@/components/icons/KeyboardIcon.vue'
+import KeyboardBase from '@/components/KeyboardBase.vue'
 
 const emit = defineEmits<{
   addToCart: [count: number]
@@ -24,30 +26,74 @@ function addToCart(): void {
   emit('addToCart', count.value)
   count.value = 0
 }
+
+const isOpenKeyboard: Ref<boolean> = ref(false)
+
+function changeOpenKeyboard() {
+  isOpenKeyboard.value = !isOpenKeyboard.value
+}
+
+function addNumber(num: number) {
+  if (count.value === 0) {
+    count.value = num
+  } else {
+    count.value = Number(`${count.value}${num}`)
+  }
+}
+
+function removeNumber() {
+  const str = String(count.value)
+
+  if (str.length === 1) {
+    count.value = 0
+  } else {
+    count.value = Math.trunc(count.value / 10)
+  }
+}
+
+function onClickAway() {
+  console.log('11')
+  changeOpenKeyboard()
+}
 </script>
 
 <template>
   <div class="actions">
+    <button class="button button--square" @click="changeOpenKeyboard">
+      <KeyboardIcon />
+    </button>
     <div class="count">{{ count }}</div>
     <button class="button button--square" @click="increment">+</button>
     <button class="button button--square" :disabled="disabledButton" @click="decrement">-</button>
     <button class="button button--square" :disabled="disabledButton" @click="addToCart">
       <CartIcon />
     </button>
+
+    <div v-if="isOpenKeyboard" v-click-away="onClickAway" class="keyboard-container">
+      <KeyboardBase @add="addNumber" @remove="removeNumber" />
+    </div>
   </div>
 </template>
 
 <style scoped>
 .actions {
+  position: relative;
   display: flex;
+}
+
+.keyboard-container {
+  position: absolute;
+  top: 40px;
+  z-index: 10;
 }
 
 .count {
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 40px;
+  min-width: 40px;
   height: 40px;
   flex: none;
+  padding: 10px;
 }
 </style>
